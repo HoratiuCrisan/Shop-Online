26527b948dfb697dfe579104eee04df580e0b124
reddsnake <horatiu.crisan01@e-uvt.ro>
2024-04-23 15:36:47 +0300

    Token required for post methods

--- public/index.php
+++ public/index.php
@@ -6,10 +6,14 @@ use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use App\Controllers\HomeController;
 use App\Controllers\ProductController;
+use App\Controllers\UserController;
+use App\Middleware\SessionMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Controllers/HomeController.php';
 require __DIR__ . '/../src/Controllers/ProductController.php';
+require __DIR__ . '/../src/Controllers/UserController.php'; // Include UserController
+require __DIR__ . '/../src/Middleware/SessionMiddleware.php';
 $dbConfig = require __DIR__ . '/../config/db_config.php';
 
 $hostname = $dbConfig['host'];
@@ -26,17 +30,28 @@ try {
 
     $app = AppFactory::create();
     $twig = Twig::create('../templates', ['cache' => false]);
-
+    $app->add(new SessionMiddleware());
     $app->get('/', [HomeController::class, 'index']);
 
-    $productController = new ProductController($db, $twig);
+    $userController = new UserController($db, $twig); // Instantiate UserControlle
+
+    // User routes
+    $app->post('/login', [$userController, 'login']); // Route for user login
+    $app->post('/register', [$userController, 'createUser']); // Route for user register
+    $app->post('/logout', [$userController, 'logout']); // Route for user logout
+    $app->get('/users', [$userController, 'getAll']); // Route to get all users
+    $app->put('/users/{username}', [$userController, 'updateUser']); // Route to update an existing user
+    $app->delete('/users/{username}', [$userController, 'deleteUser']); // Route to delete an existing user
+
+    $sessionData = $_SESSION;
+
+    $productController = new ProductController($db, $twig, $sessionData);
 
     $app->get('/products', [$productController, 'getAll']);
     $app->get('/filtered-products', [$productController, 'getByCategory']);
     $app->post('/products', [$productController, 'create']);
     $app->put('/products/{productId}', [$productController, 'update']);
     $app->delete('/products/{productId}', [$productController, 'delete']);
-    
 
     $app->add(TwigMiddleware::create($app, $twig));
 
@@ -44,5 +59,4 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-
 ?>
